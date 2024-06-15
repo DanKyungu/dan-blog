ecc83cc6d090af71ab4f6276dc387de078595070
Dan Kyungu <33684517+DanKyungu@users.noreply.github.com>
2024-06-15 11:49:45 +0000

    Add Education component

---
+++ src/Components/Education.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+/**
+ * Education
+ *
+ * Below is a sample education section. Feel free to update this to include
+ * your education history.
+ */
+const Education = () => {
+    return (
+      <section className="padding" id="education">
+        <h2 style={{ textAlign: "center" }}>Education</h2>
+        <div className="container">
+          <div className="box">
+            <h3 style={{ flexBasis: "40px" }}>University of Don Bosco</h3>
+            <p className="small">Bachelor of Science in Computer Science</p>
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Education;
+  // 
+  // export default Portfolio;
+  //
